8ad1df687592f0c68fa0bc833e5308c35d2ef663
Samuel Aladegbaiye <aladegbaiyesarmmie@gmail.com>
2025-09-04 11:45:05 +0100

    Ran DE and Enrichment Analysis

--- deseq_metastasis_analysis.R
+++ deseq_metastasis_analysis.R
@@ -26,23 +26,23 @@ GDCdownload(query, files.per.chunk = 165)
 skcm_data <- GDCprepare(query, 
                         summarizedExperiment = TRUE, #Default
                         save=TRUE, 
-                        save.filename = 'TCGA_SKCM.rda')
+                        save.filename = 'TCGA_SKCM2.rda')
 View(skcm_data)
 
 
-# Count matrix
+# Retrieving Count matrix
 expression_count <- assay(skcm_data)
 View(expression_count)
 
 
-#Sample_info
+# Retrieving Sample info
 sample_info <- colData(skcm_data) |>
   as.data.frame() |>
   apply(2, as.character) |>
   as.data.frame()
 View(sample_info)
 
-#Subsetting sample_info and removing rows with NA
+#Subsetting sample_info
 sample_info <- sample_info %>% 
   select(barcode,
          patient,
@@ -96,9 +96,132 @@ dim(expression_count)
 table(rownames(working_sample_info) %in% colnames(expression_count))
 table(rownames(working_sample_info) == colnames(expression_count))
 
-#Save your datasets into scv files
-write.csv(working_sample_info, "working_sample_info.csv")
-write.csv(expression_count, "expression_count.csv")
+#Save your datasets into rds files. This preserves structure and object types when loading back into R
+saveRDS(working_sample_info, "working_sample_info.rds")
+saveRDS(expression_count, "expression_count.rds")
+
+#Read back your data into R
+working_sample_info <- readRDS("working_sample_info.rds")
+expression_count <- readRDS("expression_count.rds")
+
+#Checking if matching was done correctly and in the right order
+table(rownames(working_sample_info) %in% colnames(expression_count))
+table(rownames(working_sample_info) == colnames(expression_count))
+
+#Filtering out lowly expressed genes
+expression_count <- expression_count[rowMeans(expression_count) >= 10, ]
+dim(expression_count)
+
+####################################################################################
+######################## DIFFERENTIAL EXPRESSION ANALYSIS ##########################
+####################################################################################
+
+#Creating a DESeq2 Object
+dds <- DESeqDataSetFromMatrix(
+  countData = expression_count,
+  colData = working_sample_info,
+  design = ~ tumor_descriptor
+)
+
+#Setting reference levels
+dds$tumor_descriptor <- relevel(dds$tumor_descriptor, ref = "Primary")
+
+#Collapse technical replicates if you have them before running DESeq
+#Running DESeq proper
+dds <- DESeq(dds)
+
+#Stabilizing your dds result. This will be used for PCA and Clustering later on
+dds_norm <- varianceStabilizingTransformation(dds)
+
+#Save and view result statistics
+res <- results(dds)
+res
+summary(res)
+
+saveRDS(res, file = "res.rds")
+
+#Setting FDR threshold to 0.01
+res0.01 <- results(dds, alpha = 0.01)
+summary(res0.01)
+
+resultsNames(dds) #To check the different checked conditions
+
+#In cases of more than two compared conditions and you want to check between two specific conditions
+#e.g.: Primary, Metastatic, Recurrent
+#Essentially saying log2foldchange = Metastatic - Recurrent
+#results(dds, contrast = c("tumor_descriptor", "Metastatic", "Recurrent"))
+
+#Filtering the most significantly differentiated genes
+res <- res[res$padj < 0.01 & abs(res$log2FoldChange) > 2, ]
+
+res_up <- res[order(res$log2FoldChange, decreasing = T), ]
+top_upregulated_genes <- head(res_up, 20) %>% as.data.frame()
+top_upregulated_genes <- rownames(top_upregulated_genes)
+
+res_down <- res[order(res$log2FoldChange), ]
+top_downregulated_genes <- head(res_down, 20) %>% as.data.frame()
+top_downregulated_genes <- rownames(top_downregulated_genes)
+
+#Saving your genes to csv files
+write.csv(top_upregulated_genes, file = "top_upregulates_genelist.csv")
+write.csv(top_downregulated_genes, file = "top_downregulates_genelist.csv")
+
+####################################################################################
+######################### FUNCTIONAL ENRICHMENT ANALYSIS ###########################
+####################################################################################
+library(clusterProfiler)
+library(org.Hs.eg.db)  
+
+# Extract gene IDs
+genes <- rownames(res)
+
+# Remove the Ensembl version numbers after the dot as bitr can't recognise them
+genes <- gsub("\\..*", "", genes)
+
+# Convert Ensembl IDs to Entrez IDs as enrichKEGG only accepts Entrez gene IDs
+gene_conversion <- bitr(genes, 
+                        fromType = "ENSEMBL", 
+                        toType = "ENTREZID", 
+                        OrgDb = org.Hs.eg.db
+                        )
+View(gene_conversion)
+
+# Create gene list for enrichment
+entrez_gene_list <- gene_conversion$ENTREZID
+
+enrich_go <- enrichGO(gene          = entrez_gene_list,
+                OrgDb         = org.Hs.eg.db,
+                ont           = "BP",         # Biological Process, could also be Cellular Component (CC), or Molecular Function (MF)
+                pAdjustMethod = "BH",         
+                pvalueCutoff  = 0.05,         
+                qvalueCutoff  = 0.05,         
+                readable      = TRUE)  # This converts Entrez IDs back to ENSEMBL gene symbols
+
+View(enrich_go)
+
+saveRDS(enrich_go, "enrich_go_results.rds")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
